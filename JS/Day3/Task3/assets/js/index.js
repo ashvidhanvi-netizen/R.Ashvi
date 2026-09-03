cbf53f09bb6494f949883568002961c4133739f5
Ashvi <ashvidhanvi@gmail.com>
2026-09-03 21:39:57 +0530

    Refactor console logs for age comparisons

--- JS/Day3/Task3/assets/js/index.js
+++ JS/Day3/Task3/assets/js/index.js
@@ -1,8 +1,20 @@
 let age = 22;
 
-console.log("age == 22:", age == 22);
-console.log("age === 22:", age === 22);
-console.log("age > 18:", age > 18);
-console.log("age < 18:", age < 18);
-console.log("age >= 18:", age >= 18);
-console.log("age <= 18:", age <= 18);
+
+age == 22;
+console.log(age==22);
+
+age === 22;
+console.log(age===22);
+
+age > 18;
+console.log(age>18);
+
+age < 18;
+console.log(age<18);
+
+age >= 18;
+console.log(age>=18);
+
+age <= 18;
+console.log(age<= 18 );
